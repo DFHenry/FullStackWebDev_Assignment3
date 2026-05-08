8ee0f6589038630298146a98a857ed958382fc5a
David Henry <david_freeman_henry@hotmail.com>
2026-05-08 17:43:44 -0400

    changed alt text on footer icons

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,10 +6,10 @@ export default function Footer()
         <div class="footer">
             <h3>The Bad Ass Baking Company</h3>
             <div class="socials">
-                <a id="bluesky" href="https://bsky.app/"><img src="../src/assets/img/icon_bluesky.svg" alt="bluesky icon" /></a>
-                <a id="instagram" href="https://www.instagram.com/"><img src="../src/assets/img/icon_instagram.svg" alt="instagram icon" /></a>
-                <a id="snapchat" href="https://www.pinterest.com/"><img src="../src/assets/img/icon_pinterest.svg" alt="pinterest icon" /></a>
-                <a id="email" href="mailto:orders@test.com"><img src="../src/assets/img/icon_email.svg" alt="email icon" /></a>
+                <a id="bluesky" href="https://bsky.app/"><img src="../src/assets/img/icon_bluesky.svg" alt="bluesky" /></a>
+                <a id="instagram" href="https://www.instagram.com/"><img src="../src/assets/img/icon_instagram.svg" alt="instagram" /></a>
+                <a id="snapchat" href="https://www.pinterest.com/"><img src="../src/assets/img/icon_pinterest.svg" alt="pinterest" /></a>
+                <a id="email" href="mailto:orders@test.com"><img src="../src/assets/img/icon_email.svg" alt="email" /></a>
             </div>
             <p>&copy; David Henry 2026.</p>
         </div>
