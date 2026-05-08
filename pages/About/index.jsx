e288f592d2bd44dc7b4115efcda898b40306566d
David Henry <david_freeman_henry@hotmail.com>
2026-05-07 20:55:06 -0400

    added social links to footer, edited cards for services, adjusted about page for different screen sizes

--- pages/About/index.jsx
+++ pages/About/index.jsx
@@ -11,19 +11,16 @@ export default function About()
         <>
             <h1 class="aboutTitle">About The Bad Ass Bakery</h1>
             <div class="aboutGrid">
-                <div class="aboutText">
-                    <p>Our bakery started as the West End Bakery over 20 years ago by my father, John Smith. On his retirement, he handed over the bakery
-                        to me, but he didn't want the business to stay the same has he had it. He told me to make it my own.</p>
-                    <br />
-                    <p>I spent a lot of my days in my young adult life in the punk scene, and I wanted this business to evoke those days as well as stand
-                        out from the other buisnesses in the area. So we've rebanded to the Bad Ass Baking Company!</p>
-                    <br />
-                    <p>To the regulars of the West End Bakery, we're still here! We might look a little edgy, but we're still serving the same baked goods
-                        and catering services we've always had, with some great new products we think you'll love just as much!</p>
-                </div>
-                <div class="aboutImage">
-                    <img src="../src/assets/img/BaB_StorefrontMockup.jpg" alt="" />
-                </div>
+                <p class="aboutText1">Our bakery started as the West End Bakery over 20 years ago by my father, John Smith. On his retirement, he handed over the bakery
+                    to me, but he didn't want the business to stay the same has he had it. He told me to make it my own.</p>
+                <br />
+                <p class="aboutText2">I spent a lot of my days in my young adult life in the punk scene, and I wanted this business to evoke those days as well as stand
+                    out from the other buisnesses in the area. So we've rebanded to the Bad Ass Baking Company!</p>
+                <br />
+                <p class="aboutText3">To the regulars of the West End Bakery, we're still here! We might look a little edgy, but we're still serving the same baked goods
+                    and catering services we've always had, with some great new products we think you'll love just as much!</p>
+            
+                <img class="aboutImage" src="../src/assets/img/BaB_StorefrontMockup.jpg" alt="" />
             </div>
 
             
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,6 +4,13 @@ export default function Footer()
 {
     return(
         <div class="footer">
+            <h3>The Bad Ass Baking Company</h3>
+            <div class="socials">
+                <a id="bluesky" href="https://bsky.app/"><img src="../src/assets/img/icon_bluesky.svg" alt="bluesky icon" /></a>
+                <a id="instagram" href="https://www.instagram.com/"><img src="../src/assets/img/icon_instagram.svg" alt="instagram icon" /></a>
+                <a id="snapchat" href="https://www.pinterest.com/"><img src="../src/assets/img/icon_pinterest.svg" alt="pinterest icon" /></a>
+                <a id="email" href="mailto:orders@test.com"><img src="../src/assets/img/icon_email.svg" alt="email icon" /></a>
+            </div>
             <p>&copy; David Henry 2026.</p>
         </div>
     )
